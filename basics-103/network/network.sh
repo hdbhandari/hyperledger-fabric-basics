c0f19e291da514297a0a693f8111f5570de39414
hdbhandari <himanshubhandari5@yahoo.co.in>
2022-02-26 21:48:15 +0530

    cryptogen tool to generate crypto material

---
+++ basics-103/network/network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+../bin/cryptogen generate --config=../organizations/cryptogen/crypto-config-orderer.yaml
+
+../bin/cryptogen generate --config=../organizations/cryptogen/crypto-config-org1.yaml
+
+../bin/cryptogen generate --config=../organizations/cryptogen/crypto-config-org2.yaml
+
+../bin/cryptogen extend --config=../organizations/cryptogen/crypto-config-orderer-peer-extend.yaml
